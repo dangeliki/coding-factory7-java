960c5e736e86a6c1165a77cb3eade4da7c0be858
dangeliki <angeliki.donta@outlook.com>
2024-12-17 20:59:26 +0200

    ch12

---
+++ src/gr/aueb/cf/ch12/Student.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch12;
+
+public class Student {
+//    Default χαρακτηρισμος (access modifier) : package private
+
+//    Access Modifiers:
+//    Public ειναι προσπελασιμα / ορατα απο ολους - java world -
+//    Private προσπελασιμα μονο μεσα απο την ιδια κλαση
+//    Protected προσπελασιμο απο την ιδια κλαση αλλα και απο υποκλασεις σε ιεραρχια κληρονομικοτητας
+//    Package private τα πεδια ειναι προσπελασιμα απο την κλαση αλλα και απο το ιδιο package
+
+    int id;
+    String firstname;
+    String lastname;
+
+}
---
+++ src/gr/aueb/cf/ch12/StudentDemo.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.ch12;
+
+public class StudentDemo {
+    public static void main(String[] args) {
+//        Δηλώνω ενα object / instance
+//        Η ειδικη μεθοδος που εχει το ιδιο ονομα με την κλαση , ονομαζεται constructor (πχ το Student()) και αρχικοποιεί τα πεδία του instance
+        Student alice = new Student();
+
+    }
+}

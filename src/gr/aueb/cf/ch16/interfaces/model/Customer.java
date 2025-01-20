dba2c899bb7864d353b951860c7f27ff9700718a
dangeliki <angeliki.donta@outlook.com>
2025-01-20 18:45:59 +0200

    cf7

--- src/gr/aueb/cf/ch16/interfaces/model/Customer.java
+++ src/gr/aueb/cf/ch16/interfaces/model/Customer.java
@@ -13,6 +13,69 @@ public class Customer {
 
     }
 
+    public Customer(Long id, String vat, String region, String address, String lastname, String firstname, String phoneNumber) {
+        this.id = id;
+        this.vat = vat;
+        this.region = region;
+        this.address = address;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.phoneNumber = phoneNumber;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public void setVat(String vat) {
+        this.vat = vat;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getVat() {
+        return vat;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 }

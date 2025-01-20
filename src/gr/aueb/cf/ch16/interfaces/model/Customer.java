8299d949bf6afecbb9f74aa463b6ef18bd43c0d4
dangeliki <angeliki.donta@outlook.com>
2025-01-20 18:33:55 +0200

    cf7

---
+++ src/gr/aueb/cf/ch16/interfaces/model/Customer.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.ch16.interfaces.model;
+
+public class Customer {
+    private Long id;
+    private String vat;
+    private String region;
+    private String address;
+    private String lastname;
+    private String firstname;
+    private String phoneNumber;
+
+    public Customer() {
+
+    }
+
+
+
+}

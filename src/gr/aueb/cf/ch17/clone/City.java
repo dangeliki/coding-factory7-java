2b149c66d020154e9f14eeb867f12be19b978441
dangeliki <angeliki.donta@outlook.com>
2025-01-21 21:03:43 +0200

    cf7

---
+++ src/gr/aueb/cf/ch17/clone/City.java
@@ -0,0 +1,40 @@
+
+package gr.aueb.cf.ch17.clone;
+
+public class City implements Cloneable {
+
+//    Παροτι δεν ειναι final δεν μπορει να αλλάξει
+    private String description;
+
+    public City() {
+
+    }
+
+    public City(String description) {
+        this.description = description;
+    }
+
+    public City(City city) {
+        this.description = city.getDescription();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    protected City clone() throws CloneNotSupportedException {
+        return (City) super.clone();
+    }
+}

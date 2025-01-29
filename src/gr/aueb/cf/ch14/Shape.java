5ddc3eb6507772cfd65eca1efa962e1f1b94d41e
dangeliki <angeliki.donta@outlook.com>
2025-01-29 21:14:54 +0200

    cf7

---
+++ src/gr/aueb/cf/ch14/Shape.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch14;
+
+/**
+ * POJO Class
+ * Δεν κληρονομεί τίποτα
+ */
+
+public class Shape {
+    public void draw() {
+        System.out.println("Drawing a shape");
+    }
+}

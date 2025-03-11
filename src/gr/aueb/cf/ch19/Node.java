a5265574c4a51bcd49ed45003edc8e4732392cf3
dangeliki <angeliki.donta@outlook.com>
2025-03-11 19:39:02 +0200

    cf7

---
+++ src/gr/aueb/cf/ch19/Node.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch19;
+
+public class Node {
+    private int item;
+
+
+
+//    Getter
+    public int getItem() {
+        return item;
+    }
+//    Setter
+    public void setItem(int item) {
+        this.item = item;
+    }
+}

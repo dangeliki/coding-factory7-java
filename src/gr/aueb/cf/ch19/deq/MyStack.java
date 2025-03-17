9a41a46b87dafccb4af3cc867f27ee43bd793e8a
dangeliki <angeliki.donta@outlook.com>
2025-03-17 20:29:40 +0200

    cf7

---
+++ src/gr/aueb/cf/ch19/deq/MyStack.java
@@ -0,0 +1,34 @@
+package gr.aueb.cf.ch19.deq;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.function.Consumer;
+
+/**
+ * LIFO impl with deque
+ */
+public class MyStack<T> {
+    private final Deque<T> myStack = new LinkedList<>();
+
+    public MyStack() {
+
+    }
+
+//    Getter
+    public Deque<T> getMyStack() {
+        return myStack;
+    }
+
+    public void push(T t) {
+        myStack.push(t);
+    }
+
+    public T pop(){
+        return myStack.pop();
+    }
+
+    public void forEach(Consumer<T> action) {
+        myStack.forEach(action);
+    }
+
+}

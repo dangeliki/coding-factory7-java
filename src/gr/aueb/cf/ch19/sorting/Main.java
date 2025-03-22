4327619ce5c23883426f373c2a9858e1fd24c8c9
dangeliki <angeliki.donta@outlook.com>
2025-03-22 17:35:52 +0200

    cf7

--- src/gr/aueb/cf/ch19/sorting/Main.java
+++ src/gr/aueb/cf/ch19/sorting/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
         List<Product> products2 = Arrays.asList(new Product("", 12.5, 10), new Product("", 12.5, 10));   //επιστρέφει partially immutable list (επιτρέπει update)
 
-        List<Product> products = new ArrayList<>(List.of(new Product("",12.5,10), new Product("oranges",12.5,2)));    //mutable list
+        List<Product> products = new ArrayList<>(List.of(new Product("apples",12.5,17), new Product("oranges",12.5,2),new Product("berries",12.5,10),new Product("apples",32.5,20)));    //επιστρέφει mutable list
 
         products.sort(Comparator.naturalOrder());   //sort based on comparable
         products.sort(Comparator.reverseOrder());   //based on comparable
@@ -20,6 +20,10 @@ public class Main {
         //εφοσον ως παραμετρος υπαρχει interface, μπορω να υλοποιησω με lamda
         //και οταν υπαρχουν τα ιδια πραγματα δεξια και αριστερα μπορουμε να παρουμε method reference
         products.sort(Comparator.comparing(Product::getName));
+        products.sort(Comparator.comparing(Product::getName).thenComparing(Product::getQuantity));
+        products.sort(Comparator.comparing(Product::getName).thenComparing(Product::getQuantity).thenComparing(Product::getPrice).reversed());
+        products.sort(Comparator.comparing(Product::getName).thenComparing(Product::getQuantity,Comparator.reverseOrder()));
 
+        products.forEach(System.out::println);
     }
 }

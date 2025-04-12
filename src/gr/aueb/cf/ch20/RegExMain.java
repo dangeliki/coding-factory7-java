ebd3ec419fb4a3e1d6d0da91a5fdea67675b986c
dangeliki <angeliki.donta@outlook.com>
2025-04-12 12:40:39 +0300

    cf7

--- src/gr/aueb/cf/ch20/RegExMain.java
+++ src/gr/aueb/cf/ch20/RegExMain.java
@@ -5,11 +5,26 @@ public class RegExMain {
 
        String s = "red";
         System.out.println(isRed(s));
+        System.out.println(isRedOrGreen(s));
 
     }
 
+    /**
+     * Ελέγχει (matches) αν το input string είναι το "red"
+     * @param s
+     * @return
+     */
     public static boolean isRed(String s) {
         return s.matches("red");
+    }
+
+    /**
+     * Ελέγχει (matches) αν το input string είναι "red" ή "green"
+     * @param s
+     * @return
+     */
+    public static boolean isRedOrGreen(String s){
+        return s.matches("red|green");
     }
 
 }

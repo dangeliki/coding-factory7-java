6ff810b524316c44c57276906bb12bb97d32cbfe
dangeliki <angeliki.donta@outlook.com>
2025-04-13 21:53:11 +0300

    cf7

---
+++ src/gr/aueb/cf/ch20/DateMatcher.java
@@ -0,0 +1,29 @@
+package gr.aueb.cf.ch20;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DateMatcher {
+    public static void main(String[] args) {
+
+        String date = "07/10/1998";     //mm/dd/yyyy
+
+        Pattern pattern = Pattern.compile("(\\d{2})/(\\d{2})/(\\d{4})");
+        Matcher matcher = pattern.matcher(date);
+
+        if (matcher.matches()){
+//            Το group σκέτο ή group(0) είναι όλο το match
+            String month = matcher.group(1);
+            String day = matcher.group(2);
+            String year = matcher.group(3);
+
+            String grDate = day + "/" + month + "/" + year;
+            System.out.println("GR Date: " +grDate);
+
+        }
+        else {
+            System.out.println("Pattern not matches.");
+        }
+
+    }
+}
